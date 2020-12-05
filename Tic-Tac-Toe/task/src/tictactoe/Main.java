db1ab481e4f32f57280625625c3a49e495deb629
Gennadiy Koganovich <genakoganovich@gmail.com>
2020-12-05 20:00:42 +0200

    0002 stage 2/5 done

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,9 +1,28 @@
 package tictactoe;
+import java.util.Scanner;
 
 public class Main {
+    public static final int SIZE = 3;
+
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        System.out.print("Enter cells: ");
+        String input = new Scanner(System.in).next();
+        for (int i = 0; i < SIZE * SIZE; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(input.charAt(i * SIZE + j) + " ");
+            }
+            System.out.println("|");
+        }
+
+        for (int i = 0; i < input.length(); i++) {
+            System.out.print("-");
+        }
+        System.out.println();
     }
 }
