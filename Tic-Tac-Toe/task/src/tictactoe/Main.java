1730d34b346d0b37e4027dd8abea050bc4c181e0
Gennadiy Koganovich <genakoganovich@gmail.com>
2020-12-09 00:18:37 +0200

    0004 stage 4/5 done

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -24,9 +24,11 @@ public class Main {
 
 
         //======================================================
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter cells: ");
-        String input = new Scanner(System.in).next();
-
+        String input = scanner.next();
+        char[] cells = input.toCharArray();
+        scanner.nextLine();
         //=======================================================
         // print the field
         for (int i = 0; i < SIZE * SIZE; i++) {
@@ -37,12 +39,12 @@ public class Main {
         for (int i = 0; i < SIZE; i++) {
             System.out.print("| ");
             for (int j = 0; j < SIZE; j++) {
-                System.out.print(input.charAt(i * SIZE + j) + " ");
+                System.out.print(cells[i * SIZE + j] + " ");
             }
             System.out.println("|");
         }
 
-        for (int i = 0; i < input.length(); i++) {
+        for (int i = 0; i < cells.length; i++) {
             System.out.print("-");
         }
         System.out.println();
@@ -52,7 +54,7 @@ public class Main {
         int[] rowSum = new int[SIZE];
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                rowSum[i] += input.charAt(j + i * SIZE);
+                rowSum[i] += cells[j + i * SIZE];
             } // for j
         } // for i
 
@@ -61,7 +63,7 @@ public class Main {
         int[] colSum = new int[SIZE];
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                colSum[i] += input.charAt(i + j * SIZE);
+                colSum[i] += cells[i + j * SIZE];
             } // for j
         }
 
@@ -69,16 +71,16 @@ public class Main {
         // calculate sum in diagonals
         int[] sumDiag = new int[2];
         for (int j = 0; j < SIZE; j++) {
-            sumDiag[0] += input.charAt((SIZE + 1) * j);
+            sumDiag[0] += cells[(SIZE + 1) * j];
         }
         for (int j = 0; j < SIZE; j++) {
-            sumDiag[1] += input.charAt((SIZE - 1) + (SIZE - 1) * j);
+            sumDiag[1] += cells[(SIZE - 1) + (SIZE - 1) * j];
         }
 
         //==============================================================
         // calculate X number
         int xNumber = 0;
-        for (char c : input.toCharArray()) {
+        for (char c : cells) {
             if (c == 'X') {
                 xNumber++;
             }
@@ -87,7 +89,7 @@ public class Main {
         //==============================================================
         // calculate O number
         int oNumber = 0;
-        for (char c : input.toCharArray()) {
+        for (char c : cells) {
             if (c == 'O') {
                 oNumber++;
             }
@@ -123,7 +125,7 @@ public class Main {
         // calculate final flags
         doesXWin = hasThreeXInRow || hasThreeXInColumn || hasThreeXInDiagonal;
         doesOWin = hasThreeOInRow || hasThreeOInColumn || hasThreeOInDiagonal;
-        hasEmptyCells = input.contains("_");
+        hasEmptyCells = String.valueOf(cells).contains("_");
         if (Math.abs(xNumber - oNumber) > 1) {
             isBigDifference = true;
         }
@@ -133,16 +135,63 @@ public class Main {
 
         //==============================================================
         // print results
-        if (isImpossible) {
-            System.out.println("Impossible");
-        } else if (isGameNotFinished) {
-            System.out.println("Game not finished");
-        } else if (isDraw) {
-            System.out.println("Draw");
-        } else if (doesXWin) {
-            System.out.println("X wins");
-        } else {
-            System.out.println("O wins");
+//        if (isImpossible) {
+//            System.out.println("Impossible");
+//        } else if (isGameNotFinished) {
+//            System.out.println("Game not finished");
+//        } else if (isDraw) {
+//            System.out.println("Draw");
+//        } else if (doesXWin) {
+//            System.out.println("X wins");
+//        } else {
+//            System.out.println("O wins");
+//        }
+
+        //==============================================================
+        // get the coordinates from a user
+        String[] values;
+        int x, y;
+        int index = 0;
+        boolean areValidCoordinates = false;
+        while (!areValidCoordinates) {
+            System.out.print("Enter the coordinates: ");
+            values = scanner.nextLine().split(" ");
+            if (values.length != 2 || !values[0].matches("\\d") || !values[1].matches("\\d")) {
+                System.out.println("You should enter numbers!");
+            } else if (!"123".contains(values[0]) || !"123".contains(values[1])){
+                System.out.println("Coordinates should be from 1 to 3!");
+            } else {
+                x = Integer.parseInt(values[0]);
+                y = Integer.parseInt(values[1]);
+                index = (x - 1) + (SIZE - y) * SIZE;
+                if (cells[(x - 1) + (SIZE - y) * SIZE]!= '_') {
+                    System.out.println("This cell is occupied! Choose another one!");
+                } else {
+                    areValidCoordinates = true;
+                }
+            }
+        } // while
+        cells[index] = 'X';
+
+        //=======================================================
+        // print the field again
+        for (int i = 0; i < SIZE * SIZE; i++) {
+            System.out.print("-");
         }
-    }
-}
+        System.out.println();
+
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(cells[i * SIZE + j] + " ");
+            }
+            System.out.println("|");
+        }
+
+        for (int i = 0; i < cells.length; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+
+    } // main
+} // class Main
